5110e5b717c426853599cf558e34afd8405603e4
Vera Furtula <verafurtula@gmail.com>
2017-11-19 15:06:36 +0100

    bla

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :coveks
   get 'pages/info'
+  root to: redirect('/coveks')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
